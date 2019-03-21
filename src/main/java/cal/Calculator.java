3c8e38b87b339b0e45778cadfabb57352c7f76ad
kylie.do <kylie.do@kakaocorp.com>
2019-03-22 04:26:17 +0900

    add build.gradle

---
+++ src/main/java/cal/Calculator.java
@@ -0,0 +1,3 @@
+package cal;
+
+public class Calculator {}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import cal.Calculator;
+
+public class CalculatorTest {
+
+    @Test
+    public void test() {
+        Calculator calculator = new Calculator();
+    }
+}
